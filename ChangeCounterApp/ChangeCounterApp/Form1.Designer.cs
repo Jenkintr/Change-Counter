f773049785730a9a22cf3f6a8c1405754e40f877
Jenkintr <Jenkintr@users.noreply.github.com>
2016-09-14 12:47:31 -0500

    Add files via upload

---
+++ ChangeCounterApp/ChangeCounterApp/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ChangeCounterApp
+{
+    partial class changeCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.oneCentPictureBox = new System.Windows.Forms.PictureBox();
+            this.fiveCentsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tenCentsPictureBox = new System.Windows.Forms.PictureBox();
+            this.twentyFiveCentsPictureBox = new System.Windows.Forms.PictureBox();
+            this.outputDiscriptionLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.oneCentPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fiveCentsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenCentsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twentyFiveCentsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(87, 9);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(110, 16);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Click the Coins";
+            // 
+            // oneCentPictureBox
+            // 
+            this.oneCentPictureBox.Image = global::ChangeCounterApp.Properties.Resources.penny;
+            this.oneCentPictureBox.Location = new System.Drawing.Point(12, 28);
+            this.oneCentPictureBox.Name = "oneCentPictureBox";
+            this.oneCentPictureBox.Size = new System.Drawing.Size(122, 122);
+            this.oneCentPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.oneCentPictureBox.TabIndex = 1;
+            this.oneCentPictureBox.TabStop = false;
+            this.oneCentPictureBox.Click += new System.EventHandler(this.oneCentPictureBox_Click);
+            // 
+            // fiveCentsPictureBox
+            // 
+            this.fiveCentsPictureBox.Image = global::ChangeCounterApp.Properties.Resources.nickle;
+            this.fiveCentsPictureBox.Location = new System.Drawing.Point(140, 28);
+            this.fiveCentsPictureBox.Name = "fiveCentsPictureBox";
+            this.fiveCentsPictureBox.Size = new System.Drawing.Size(122, 122);
+            this.fiveCentsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fiveCentsPictureBox.TabIndex = 2;
+            this.fiveCentsPictureBox.TabStop = false;
+            this.fiveCentsPictureBox.Click += new System.EventHandler(this.fiveCentsPictureBox_Click);
+            // 
+            // tenCentsPictureBox
+            // 
+            this.tenCentsPictureBox.Image = global::ChangeCounterApp.Properties.Resources.dime;
+            this.tenCentsPictureBox.Location = new System.Drawing.Point(12, 156);
+            this.tenCentsPictureBox.Name = "tenCentsPictureBox";
+            this.tenCentsPictureBox.Size = new System.Drawing.Size(122, 122);
+            this.tenCentsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.tenCentsPictureBox.TabIndex = 3;
+            this.tenCentsPictureBox.TabStop = false;
+            this.tenCentsPictureBox.Click += new System.EventHandler(this.tenCentsPictureBox_Click);
+            // 
+            // twentyFiveCentsPictureBox
+            // 
+            this.twentyFiveCentsPictureBox.Image = global::ChangeCounterApp.Properties.Resources.quarter;
+            this.twentyFiveCentsPictureBox.Location = new System.Drawing.Point(140, 156);
+            this.twentyFiveCentsPictureBox.Name = "twentyFiveCentsPictureBox";
+            this.twentyFiveCentsPictureBox.Size = new System.Drawing.Size(122, 122);
+            this.twentyFiveCentsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.twentyFiveCentsPictureBox.TabIndex = 4;
+            this.twentyFiveCentsPictureBox.TabStop = false;
+            this.twentyFiveCentsPictureBox.Click += new System.EventHandler(this.twentyFiveCentsPictureBox_Click);
+            // 
+            // outputDiscriptionLabel
+            // 
+            this.outputDiscriptionLabel.AutoSize = true;
+            this.outputDiscriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDiscriptionLabel.Location = new System.Drawing.Point(315, 119);
+            this.outputDiscriptionLabel.Name = "outputDiscriptionLabel";
+            this.outputDiscriptionLabel.Size = new System.Drawing.Size(43, 15);
+            this.outputDiscriptionLabel.TabIndex = 5;
+            this.outputDiscriptionLabel.Text = "Total:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalLabel.Location = new System.Drawing.Point(284, 134);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalLabel.TabIndex = 6;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(296, 190);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(296, 163);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // changeCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 296);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.outputDiscriptionLabel);
+            this.Controls.Add(this.twentyFiveCentsPictureBox);
+            this.Controls.Add(this.tenCentsPictureBox);
+            this.Controls.Add(this.fiveCentsPictureBox);
+            this.Controls.Add(this.oneCentPictureBox);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "changeCounter";
+            this.Text = "Change Counter";
+            ((System.ComponentModel.ISupportInitialize)(this.oneCentPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fiveCentsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenCentsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twentyFiveCentsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.PictureBox oneCentPictureBox;
+        private System.Windows.Forms.PictureBox fiveCentsPictureBox;
+        private System.Windows.Forms.PictureBox tenCentsPictureBox;
+        private System.Windows.Forms.PictureBox twentyFiveCentsPictureBox;
+        private System.Windows.Forms.Label outputDiscriptionLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ ChangeCounterApp/ChangeCounterApp/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeCounterApp
+{
+    public partial class changeCounter : Form
+    {
+        //Constant fields
+        const decimal PENNY_VALUE = 0.01m;
+        const decimal NICKEL_VALUE = 0.05m;
+        const decimal DIME_VALUE = 0.10m;
+        const decimal QUARTER_VALUE = 0.25m;
+
+        //Field Variable to hold the total:
+        private decimal total = 0m;
+
+        public changeCounter()
+        {
+            InitializeComponent();
+        }
+
+        private void oneCentPictureBox_Click(object sender, EventArgs e)
+        {
+            //Add penny to total and format as currency.
+            total += PENNY_VALUE;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            //Add nickel to total & format to currency.
+            total += NICKEL_VALUE;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void tenCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            //Add dime to total & format to currency.
+            total += DIME_VALUE;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            //Add quarter to total & format to currency.
+            total += QUARTER_VALUE;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Clears the total and the output controls
+            totalLabel.Text = "";
+            total = 0m;
+        }
+    }
+}
